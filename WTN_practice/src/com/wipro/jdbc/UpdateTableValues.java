cabaa7fcb9f6f9f150f84f570d3e89e40c757739
nikhil-teki <nikhil.teki2000@gmail.com>
2020-08-18 14:02:42 +0530

    JDBC Commit 3

---
+++ WTN_practice/src/com/wipro/jdbc/UpdateTableValues.java
@@ -0,0 +1,34 @@
+package com.wipro.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class UpdateTableValues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Connecting to the Oracle Database");
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","AP@07ca1638");
+			System.out.println("Connected");
+			Statement stm=conn.createStatement();
+			String query="update student set cgpa=9.3 where id=170040857";
+			stm.executeUpdate(query);
+			query="select * from student";
+			ResultSet rs=stm.executeQuery(query);
+			System.out.println("ID\tName\tCGPA");
+			while(rs.next())
+				System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getFloat(3));
+		}
+		catch(Exception e)
+		{
+			System.out.println("Connection could not be established");
+			System.out.println(e);
+		}
+	}
+
+}
